eac03e3ae4b182ed4c54cef2b983ba52ec7b5e7e
garlicvread <megaflops@naver.com>
2022-05-31 19:42:26 +0900

    Initial Commit

---
+++ kko_okk/kko_okkApp.swift
@@ -0,0 +1,20 @@
+//
+//  kko_okkApp.swift
+//  kko_okk
+//
+//  Created by Kim, Che-Pill on 2022/05/31.
+//
+
+import SwiftUI
+
+@main
+struct kko_okkApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
