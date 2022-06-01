ac5a99e790c8e3d4138c941ebf6a24ca9098f27b
yeni <yeniful@icloud.com>
2022-06-01 13:33:04 +0900

    Style[#10] : Style 코드를 추가했습니다.

--- kko_okk/Views/BoardView.swift
+++ kko_okk/Views/BoardView.swift
@@ -1,5 +1,5 @@
 //
-//  HeaderView.swift
+//  BoardView.swift
 //  kko_okk
 //
 //  Created by Yeni Hwang on 2022/06/01.
@@ -7,14 +7,14 @@
 
 import SwiftUI
 
-struct HeaderView: View {
+struct BoardView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct HeaderView_Previews: PreviewProvider {
+struct BoardView_Previews: PreviewProvider {
     static var previews: some View {
-        HeaderView()
+        BoardView()
     }
 }
--- kko_okk/Views/MainView.swift
+++ kko_okk/Views/MainView.swift
@@ -2,7 +2,19 @@
 //  MainView.swift
 //  kko_okk
 //
-//  Created by Kim, Che-Pill on 2022/05/31.
+//  Created by Yeni Hwang on 2022/06/01.
 //
 
-import Foundation
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
