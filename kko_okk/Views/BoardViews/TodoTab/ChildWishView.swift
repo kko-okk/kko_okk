2a549a710b67c3784b41e1d59056fe34d51313d5
garlicvread <try2quit@korea.ac.kr>
2022-06-02 13:51:20 +0900

    [#16] ParentWishView, ChildWishView, ContractView, ButtonForContract 네 개 파일 업데이트

--- kko_okk/Views/BoardViews/TodoTab/ChildWishView.swift
+++ kko_okk/Views/BoardViews/TodoTab/ChildWishView.swift
@@ -15,6 +15,18 @@ struct ChildWishView: View {
                 TextField("아이용", text: .constant(""))
                     .padding(.leading, 10)
                 
+                Circle()
+                    .fill(Color.gray.opacity(0.4))
+                    .frame(width: 20, height: 20)
+                    .overlay(
+                        // 원 안의 숫자는 state-binding으로 컬럼 내 액티브 버튼 갯수가 저장된 변수에 묶어야 함. 지금은 임시로 넣어 놓은 상태.
+                        Text("1")
+                            .font(.system(size: 10))
+                    )
+                
+                Spacer()
+                    .frame(width: 50, height: 30)
+                
                 Button(action: {
                     print("Add button tapped")
                 }) {
@@ -25,7 +37,6 @@ struct ChildWishView: View {
             .padding(.trailing, 10)
 
             Divider()
-            Spacer()
         }
     }
 }
