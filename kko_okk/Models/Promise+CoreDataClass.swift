6359611420e8ca53cbba355d4423ab894043165e
Valentino1994 <demua1994@gmail.com>
2022-06-03 15:28:26 +0900

    Fix[#23] Core Data Delete 버그를 수정했습니다.

---
+++ kko_okk/Models/Promise+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Promise+CoreDataClass.swift
+//  kko_okk
+//
+//  Created by Geunil Park on 2022/06/02.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Promise)
+public class Promise: NSManagedObject {
+
+}
