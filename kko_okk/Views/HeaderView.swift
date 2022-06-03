3d4f03838d6f839b47d4bf881bedc66be511efd7
RuyHa <code.ruyha@gmail.com>
2022-06-03 15:23:29 +0900

    Feat[#26] : HeaderView/Tilteview 초안완성

--- kko_okk/Views/HeaderView.swift
+++ kko_okk/Views/HeaderView.swift
@@ -8,16 +8,37 @@
 import SwiftUI
 
 struct HeaderView: View {
+    let HVA = HeaderViewAssets()
+    
     var body: some View {
+        ZStack{
+            Color.gray
+            // ZStack과 배경색을 대충 넣은이유는ㄴ
+            // TitleView와 RuyhaTestVeiw의 배경색을 작업할때 같은 흰색이면 끔찍해서
+            // 구분하기 위해 넣었습니다. 추후 삭제 예정
+            
+            HStack{
+                TitleView()
+//                    .cornerRadius(HVA.cornerRadius)
+                    .frame(width: 965, height: 180, alignment: .leading)
+                    .padding(.trailing,25)
+                
+                RuyhaTestVeiw()
+                    .cornerRadius(HVA.cornerRadius)
+                    .frame(width: 300, height: 145, alignment: .bottomTrailing)
+                    .padding(.top,35)
+            }//HStack
+            .padding(.top,50)
+        }//ZStack
+        .frame(minWidth: 0,maxWidth: .infinity, minHeight: 0,maxHeight: 200, alignment: .bottomLeading)
+        //손가락 모양이박스 위로 돌출 되는 형태인데 아직 정확한 높이가 나오지 않아서 임시 값을 넣어 놨습니다.
 
-        Rectangle().frame(width: Style.ViewSize.HeaderSize.width, height: Style.ViewSize.HeaderSize.height)
-        
-        TitleView()
-    }
+    }//View
 }
 
 struct HeaderView_Previews: PreviewProvider {
     static var previews: some View {
         HeaderView()
+            .previewInterfaceOrientation(.landscapeLeft)
     }
 }
