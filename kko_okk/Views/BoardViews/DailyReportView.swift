dfe8b483895e793d71a082d1f17aef59c924e4b8
ian <isnn0103@gmail.com>
2022-06-09 05:37:54 +0900

    Feat[#29]: DailyReport 원형 그래프 구현
    - 목업 데이터의 결과값에 따라 그래프가 유동적으로 변합니다.

--- kko_okk/Views/BoardViews/DailyReportView.swift
+++ kko_okk/Views/BoardViews/DailyReportView.swift
@@ -9,12 +9,26 @@ import SwiftUI
 
 struct DailyReportView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            ForEach(dailyReportDatas.indices, id: \.self) { index in
+                ZStack {
+                    Circle()
+                        .stroke(.gray.opacity(0.3), lineWidth: 30)
+                    Circle()
+                        .trim(from: 0, to: dailyReportDatas[index].progress / 100)
+                        .stroke(dailyReportDatas[index].keyColor, style: StrokeStyle(lineWidth: 30, lineCap: .round, lineJoin: .round))
+                        .rotationEffect(.init(degrees: -90))
+                }
+                .padding(CGFloat(index) * 37)
+            }
+        }
+        .frame(width: 170, height: 170)
     }
 }
 
 struct DailyReportView_Previews: PreviewProvider {
     static var previews: some View {
         DailyReportView()
+            .previewInterfaceOrientation(.landscapeRight)
     }
 }
