35127e316fb092b0dc8c41d2b4789577d838c850
ian <isnn0103@gmail.com>
2022-06-10 22:11:22 +0900

    Feat[#58]: WeeklyReport 목업 데이터 구현

---
+++ kko_okk/Models/MockupData/WeeklyReportData.swift
@@ -0,0 +1,43 @@
+//
+//  WeeklyReportData.swift
+//  kko_okk
+//
+//  Created by ian on 2022/06/10.
+//
+
+import SwiftUI
+
+struct ParentWeeklyReportData: Identifiable {
+    var id = UUID().uuidString
+    var value: CGFloat
+    var key: String
+    var color: Color = Color.Kkookk.parentPurple
+}
+
+struct ChildrenWeeklyReportData: Identifiable {
+    var id = UUID().uuidString
+    var value: CGFloat
+    var key: String
+    var color: Color = Color.Kkookk.childGreen
+}
+
+var parentWeeklyReportDatas: [ParentWeeklyReportData] = [
+    ParentWeeklyReportData(value: 100, key: "6월 4일"),
+    ParentWeeklyReportData(value: 100, key: "6월 5일"),
+    ParentWeeklyReportData(value: 100, key: "6월 6일"),
+    ParentWeeklyReportData(value: 100, key: "6월 7일"),
+    ParentWeeklyReportData(value: 100, key: "6월 8일"),
+    ParentWeeklyReportData(value: 100, key: "6월 9일"),
+    ParentWeeklyReportData(value: 100, key: "6월 10일")
+]
+
+var childrenWeeklyReportDatas: [ChildrenWeeklyReportData] = [
+    ChildrenWeeklyReportData(value: 100, key: "6월 4일"),
+    ChildrenWeeklyReportData(value: 100, key: "6월 5일"),
+    ChildrenWeeklyReportData(value: 100, key: "6월 6일"),
+    ChildrenWeeklyReportData(value: 100, key: "6월 7일"),
+    ChildrenWeeklyReportData(value: 100, key: "6월 8일"),
+    ChildrenWeeklyReportData(value: 100, key: "6월 9일"),
+    ChildrenWeeklyReportData(value: 100, key: "6월 10일"),
+]
+
