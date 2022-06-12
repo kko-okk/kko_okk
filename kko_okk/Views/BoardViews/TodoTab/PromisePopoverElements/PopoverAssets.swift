be74038274d7215fcc060ba1deb33fec4e7838bf
seodam-hst <seodam.hst@gmail.com>
2022-06-12 14:06:10 +0900

    [#68] 팝오버 Assets 파일, 변수명 변경

---
+++ kko_okk/Views/BoardViews/TodoTab/PromisePopoverElements/PopoverAssets.swift
@@ -0,0 +1,14 @@
+//
+//  PopoverViewAssets.swift
+//  kko_okk
+//
+//  Created by 임성균 on 2022/06/10.
+//
+
+import Foundation
+
+class PopoverAssets {
+    let popoverfullWidth = KkookkSize.fullWidth - 80 //헤더뷰 전체 길이 - 좌우 패딩 40씩
+    let popoverfullheight = KkookkSize.fullHeight * 0.1
+    let popoverCellheight = KkookkSize.fullHeight * 0.141
+}
