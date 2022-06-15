6f3db78fddd4c50ad1ed74079ed3f6fa486e839b
yeni <yeniful@icloud.com>
2022-06-16 07:30:22 +0900

    Fix[#111] : isDone check 애니메이션 위치 조정

--- kko_okk/Views/BoardViews/TodoTab/ButtonForContract.swift
+++ kko_okk/Views/BoardViews/TodoTab/ButtonForContract.swift
@@ -146,6 +146,7 @@ struct ButtonForContract: View {
                             .padding(.bottom, 5)
                             .frame(maxWidth: .infinity, alignment: .leading)
 
+                        // 수정, 삭제 Popover
                         if !contract.promised {
                             Menu {
                                 Button {
@@ -185,18 +186,15 @@ struct ButtonForContract: View {
                 // promised List에서 check 버튼활성화
                 if contract.promised {
                     Spacer()
-                    Spacer()
-                    Spacer()
                     VStack{
                         Spacer()
-//                        GeometryReader { geometry in
+                        GeometryReader { geometry in
                                 Text("P")
                                     .foregroundColor(.gray)
                                     .background(
                                         Circle()
                                             .fill(self.isDetachingParentCheck ?
-                                                             Color.pink :
-                                                        (self.completedParentCheck ? .blue : Color.Kkookk.commonWhite))
+                                                .yellow : Color.Kkookk.commonWhite)
                                             .frame(width: 35, height: 35, alignment: .center)
                                             .gesture(parentCheckGesture)
                                         ).padding(.bottom, 15)
@@ -212,33 +210,40 @@ struct ButtonForContract: View {
                                 .stroke(style: StrokeStyle(lineWidth: 8, lineCap: .round, lineJoin: .round))
                                 .offset(x: 0, y: 0)
                                 .animation(Animation.easeInOut(duration: 0.5).delay(0))
+                                .frame(width: geometry.size.width, height: geometry.size.height)
+                                .foregroundColor(Color.Kkookk.parentPurple)
                             }
+                        }
                         
-                        
+                        GeometryReader { geometry in
                             Text("C")
                                 .foregroundColor(.gray)
                                 .background(
                                     Circle()
                                         .fill(self.isDetachingChildCheck ?
-                                                     Color.pink :
-                                                (self.completedChildCheck ? .blue : Color.Kkookk.commonWhite))
+                                            .yellow : Color.Kkookk.commonWhite)
                                         .frame(width: 35, height: 35, alignment: .center)
                                         .gesture(childCheckGesture)
                                     )
-                        ZStack{
-                            Path { path in
-                                path.move(to: CGPoint(x: -1, y: -1))
-                                       path.addCurve(to: CGPoint(x: 21, y: 26), control1: CGPoint(x: -1, y: -1), control2: CGPoint(x: 22, y: 26))
-                                       path.addCurve(to: CGPoint(x: 56, y: -28), control1: CGPoint(x: 20, y: 26), control2: CGPoint(x: 56, y: -28))
-                                       path.move(to: CGPoint(x: -1, y: -1))
+                            ZStack{
+                                Path { path in
+                                    path.move(to: CGPoint(x: -1, y: -1))
+                                           path.addCurve(to: CGPoint(x: 21, y: 26), control1: CGPoint(x: -1, y: -1), control2: CGPoint(x: 22, y: 26))
+                                           path.addCurve(to: CGPoint(x: 56, y: -28), control1: CGPoint(x: 20, y: 26), control2: CGPoint(x: 56, y: -28))
+                                           path.move(to: CGPoint(x: -1, y: -1))
 
-                            }
-                            .trim(from: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, to: CGFloat(childShowCheckmark))
-                            .stroke(style: StrokeStyle(lineWidth: 8, lineCap: .round, lineJoin: .round))
-                            .offset(x: 0, y: 0)
-                            .animation(Animation.easeInOut(duration: 0.5).delay(0))
-                            }
+                                }
+                                .trim(from: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, to: CGFloat(childShowCheckmark))
+                                .stroke(style: StrokeStyle(lineWidth: 8, lineCap: .round, lineJoin: .round))
+                                .offset(x: 0, y: 0)
+                                .animation(Animation.easeInOut(duration: 0.5).delay(0))
+                                .frame(width: geometry.size.width, height: geometry.size.height)
+                                .foregroundColor(Color.Kkookk.childGreen)
+                                }
                         }
+                        Spacer()
+                    }
+                    .padding([.top, .bottom], 5)
                     .padding(.trailing, 10)
                 }
             }
