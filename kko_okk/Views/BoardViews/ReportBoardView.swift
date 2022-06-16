77dde1985a0d410dcb8a0e5df99e57a6aebfef83
RuyHa <code.ruyha@gmail.com>
2022-06-16 15:17:13 +0900

    Feat[#116] 새로운 버전과 합침

--- kko_okk/Views/BoardViews/ReportBoardView.swift
+++ kko_okk/Views/BoardViews/ReportBoardView.swift
@@ -9,22 +9,69 @@ import SwiftUI
 
 struct ReportBoardView: View {
     var body: some View {
-        HStack {
-            VStack {
-                HStack {
-                    DailyReportView()
-                    DailyReportView()
+        ZStack{
+            Color.cyan
+            GeometryReader { geometry in
+                VStack{
+                    Spacer(minLength: 20)
+                    HStack{
+                        VStack{
+                            HStack{
+                                DailyReportView()
+//                                testView1()
+                                    .cornerRadius(HeaderViewConst.shared.cornerRadius)
+                                Spacer(minLength: 20)
+                                testView2()
+                                    .cornerRadius(HeaderViewConst.shared.cornerRadius)
+                            }
+                            Spacer(minLength: 20)
+                            WeeklyReportView()
+                                .background(Color.white)
+                                .cornerRadius(HeaderViewConst.shared.cornerRadius)
+                                .frame(height: geometry.size.height * 0.54)
+                        }
+                        Spacer(minLength: 30)
+                        testView4()
+                            .cornerRadius(HeaderViewConst.shared.cornerRadius)
+                            .frame(width: geometry.size.width * 0.329)
+                    }//1246
+                    Spacer(minLength: 20)
                 }
-                WeeklyReportView()
-                Spacer()
             }
-            MonthlyReportView()
         }
     }
 }
+//이 뷰의 전체 높이 670 /366
+
+
+struct testView1: View {
+    var body: some View {
+        
+        Color.red
+    }
+}
+
+struct testView2: View {
+    var body: some View {
+        Color.green
+    }
+}
+struct testView3: View {
+    var body: some View {
+        Color.black
+    }
+}
+struct testView4: View {
+    var body: some View {
+        Color.yellow
+    }
+}
+
+
 
 struct ReportBoardView_Previews: PreviewProvider {
     static var previews: some View {
         ReportBoardView()
+            .previewInterfaceOrientation(.landscapeRight)
     }
 }
