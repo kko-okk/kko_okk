62773179a8df17931982382bb8757fbd57fc846f
RuyHa <code.ruyha@gmail.com>
2022-07-13 20:59:53 +0900

    Feat[#225] 로컬라이징 추가 이슈 확인 후 수정 설정 버튼 숨김 처리 완료

--- kko_okk/Views/SegmentView.swift
+++ kko_okk/Views/SegmentView.swift
@@ -82,17 +82,18 @@ struct SegmentView: View {
                     
                     Spacer()
                     
-                    Button(action: { isPressedSettingButton.toggle() },
-                           label: {
-                        HStack(alignment:.bottom,spacing: 1){
-                            Image(systemName: "gearshape.fill")
-                                .foregroundColor(.Kkookk.commonBlack)
-                                .font(Font.Kkookk.boardSettingButtonIcon)
-                            Text("Setting".localized)
-                                .foregroundColor(.Kkookk.commonBlack)
-                                .font(Font.Kkookk.boardSettingButton)
-                        }
-                    })
+//  TODO: Setting Button 설정버튼 주석 7.13일 -Ruyha-
+//                    Button(action: { isPressedSettingButton.toggle() },
+//                           label: {
+//                        HStack(alignment:.bottom,spacing: 1){
+//                            Image(systemName: "gearshape.fill")
+//                                .foregroundColor(.Kkookk.commonBlack)
+//                                .font(Font.Kkookk.boardSettingButtonIcon)
+//                            Text("Setting".localized)
+//                                .foregroundColor(.Kkookk.commonBlack)
+//                                .font(Font.Kkookk.boardSettingButton)
+//                        }
+//                    })
                 }
                 .padding(.vertical,15)
                 .readSize { tabSize in
