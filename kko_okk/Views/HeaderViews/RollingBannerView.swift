5ceecaa242d6b17f051cf2e410de8cd230a01be8
Kim, Raymond <82295573+garlicvread@users.noreply.github.com>
2022-07-13 11:22:41 +0900

    Educational Tips Update
    ### Motivation
    - 교육 팁 영어 로컬라이징 확대적용
    
    ### Key Changes
    - Localizable.strings 업데이트
    - TipModel.swift에서 영문 교육 팁 20개로 확대
    - 기타 눈에 보이는 whitespace 제거
    
    ### To Reviewers
    바로 머지하겠습니당.

--- kko_okk/Views/HeaderViews/RollingBannerView.swift
+++ kko_okk/Views/HeaderViews/RollingBannerView.swift
@@ -13,9 +13,9 @@ struct RollingBannerView: View {
     var rollingController = RollingBannerController()
     let time: Double = 15
     let viewCount: Int = 5
-    
-    //TODO: 팁 설정하는 부분 혼자 결정하기 뭐해서 남겨 놓은 주석들입니다.
-    //TODO: 다들 별 문제 없다고 생각하면 6/19일 이후 삭제 예정
+
+    // TODO: 팁 설정하는 부분 혼자 결정하기 뭐해서 남겨 놓은 주석들입니다.
+    // TODO: 다들 별 문제 없다고 생각하면 6/19일 이후 삭제 예정
     var body: some View {
             TabView(selection: $selectedItem){
                 ForEach((0...viewCount),id:\.self){ gz in
@@ -38,7 +38,7 @@ extension RollingBannerView{
     func playRollingBanner(){
         Timer.scheduledTimer(withTimeInterval: time, repeats: true){(Timer) in
             withAnimation(.easeInOut(duration: 1)){
-                // 하단 주석 1참고
+                // 하단 주석 1 참고
                 guard selectedItem == viewCount else {
                     selectedItem = selectedItem + 1
                     return
@@ -47,12 +47,12 @@ extension RollingBannerView{
             }
         }
     }
-    
+
     func setupAppearance() {
         UIPageControl.appearance().currentPageIndicatorTintColor = UIColor(Color(hex: "#7C7C7C"))
         UIPageControl.appearance().pageIndicatorTintColor = UIColor(Color(hex: "#D9D9D9"))
     }
-    
+
 }
 
 struct RollingBannerView_Previews: PreviewProvider {
@@ -64,6 +64,6 @@ struct RollingBannerView_Previews: PreviewProvider {
 
 /*
  코드 출처
- #주석1
+ # 주석1
  https://stackoverflow.com/questions/62864221/change-tabview-indicator-swiftui
  */
